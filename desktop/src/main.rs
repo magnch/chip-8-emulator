b40ef49920abf5cc4dce2d1896331d0cd2bb3f89
magnch <magnus.11387@gmail.com>
2026-08-13 00:07:25 +0200

    Add bare-bones test loop

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -5,8 +5,24 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 
+use chip8_core::chip8::Chip8;
+
+const CYCLES_PER_SECOND: u16 = 700;
+const TIMER_HZ: u8 = 60;
+
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
+
+    let mut chip8 = Chip8::new(); 
+
+    let rom = std::fs::read("roms/ibm_logo.ch8").expect("failed to read ROM");
+    chip8.load_rom(rom.as_slice());
+
+    loop {
+        chip8.step();
+        print_display(&chip8);
+    }
+
+    /* let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
@@ -38,5 +54,18 @@ pub fn main() {
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    } */
+}
+
+fn print_display(chip8: &Chip8) {
+    for row in chip8.get_display_content() {
+        for pixel in row {
+            if *pixel {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+        }
+        print!("\r\n");
     }
 }
