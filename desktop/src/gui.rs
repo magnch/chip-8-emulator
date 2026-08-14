827a91c7c6543e85c2b26603d2288efc3ece22c1
magnch <magnus.11387@gmail.com>
2026-08-14 19:17:57 +0200

    Restructure desktop module

---
+++ desktop/src/gui.rs
@@ -0,0 +1,60 @@
+extern crate sdl2;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
+use chip8_core::display::Display;
+
+pub struct Renderer {
+    canvas: Canvas<Window>,
+    scale: usize
+}
+
+impl Renderer {
+    pub fn new(sdl_context: &sdl2::Sdl, scale: usize) -> Self {
+        // SDL2 initialization
+        let width = (Display::WIDTH * scale) as u32;
+        let height = (Display::HEIGHT * scale) as u32;
+
+        let video_subsystem = sdl_context.video().unwrap();
+        let window = video_subsystem.window("chip-8 emulator", width, height)
+            .position_centered()
+            .build()
+            .unwrap();
+        let mut canvas = window.into_canvas().build().unwrap();
+        // Clear window
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        canvas.present();
+
+        Self { canvas, scale }
+    }
+
+    pub fn render(&mut self, display: &Display) {
+        // Clear screen and set draw color to white
+        self.canvas.set_draw_color(Color::BLACK);
+        self.canvas.clear();
+        self.canvas.set_draw_color(Color::WHITE);
+        // Draw each pixel
+        for y in 0..Display::HEIGHT {
+            for x in 0..Display::WIDTH {
+                if display.get_content()[y][x] {
+                    self.draw_pixel(x, y);
+                }
+            }
+        }
+        // Update window
+        self.canvas.present();
+    }
+
+    fn draw_pixel(&mut self, x: usize, y: usize) {
+        let pixel = Rect::new(
+            (x * self.scale) as i32,
+            (y * self.scale) as i32,
+            self.scale as u32,
+            self.scale as u32
+        );
+        self.canvas.fill_rect(pixel).unwrap();
+    }
+}
--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,50 +1,29 @@
-extern crate sdl2;
-
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-
 use std::time::Duration;
-use std::thread;
 
 use chip8_core::chip8::Chip8;
+mod gui;
+mod input;
 
 const CYCLES_PER_SECOND: u16 = 700;
 const TIMER_HZ: u8 = 60;
+const WINDOW_SCALE: usize = 10;
+
 
 pub fn main() {
 
+    // Chip-8
     let mut chip8 = Chip8::new(); 
-
     let rom = std::fs::read("roms/ibm_logo.ch8").expect("failed to read ROM");
     chip8.load_rom(rom.as_slice());
 
-    loop {
-        chip8.step();
-        print_display(&chip8);
-        // Update at 1 Hz
-        std::thread::sleep(Duration::from_secs(1));
-    }
-
-    /* let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    // Set up GUI and input
+    let sdl_context = sdl2::init().unwrap();
+    let mut renderer = gui::Renderer::new(&sdl_context, WINDOW_SCALE);
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -54,22 +33,10 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-
-        canvas.present();
+        // Game loop
+        chip8.step();
+        // Renderer
+        renderer.render(chip8.get_display());
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    } */
-}
-
-fn print_display(chip8: &Chip8) {
-    for row in chip8.get_display_content() {
-        for pixel in row {
-            if *pixel {
-                print!("*");
-            } else {
-                print!(" ");
-            }
-        }
-        print!("\r\n");
     }
 }
