34504765e26293546926d504889967800e0dce41
magnch <magnus.11387@gmail.com>
2026-08-15 12:27:50 +0200

    Add and implement most instructions

--- core/src/chip8.rs
+++ core/src/chip8.rs
@@ -1,15 +1,19 @@
 use crate::display::Display;
 use crate::memory::Memory;
+use crate::keypad::Keypad;
 use crate::opcode;
 use crate::opcode::Instruction;
+use rand;
 
 pub struct Chip8 {
     ram: Memory,
     display: Display,
-    stack: [u16; Self::STACK_SIZE],
+    keypad: Keypad,
     registers: [u8; Self::NUM_REGS],
+    stack: [u16; Self::STACK_SIZE],
+    sp: usize,
     pc: usize,
-    index: u16,
+    index: usize,
     delay_timer: u8,
     sound_timer: u8,
 }
@@ -22,8 +26,10 @@ impl Chip8 {
         Chip8 {
             ram: Memory::new(),
             display: Display::new(),
-            stack: [0; Self::STACK_SIZE],
+            keypad: Keypad::new(),
             registers: [0; Self::NUM_REGS],
+            stack: [0; Self::STACK_SIZE],
+            sp: 1,
             pc: 0,
             index: 0,
             delay_timer: 0,
@@ -68,11 +74,38 @@ impl Chip8 {
     fn execute(&mut self, instruction: Instruction) {
         match instruction {
             Instruction::Cls => self.display.clear(),
-            Instruction::Jmp(nnn) => self.pc = nnn as usize,
-            Instruction::Mov(x, nn) => self.registers[x] = nn,
-            Instruction::Add(x, nn) => self.registers[x] += nn,
+            Instruction::Rts => self.pc = self.pop() as usize,
+            Instruction::Low => (),
+            Instruction::High => (),
+            Instruction::Jmp(nnn) => self.pc = nnn,
+            Instruction::Jsr(nnn) => self.execute_jsr(nnn),
+            Instruction::Skeq_const(x, nn) => self.skip_if_eq(self.registers[x], nn),
+            Instruction::Skne_const(x, nn) => self.skip_if_not_eq(self.registers[x], nn),
+            Instruction::Skeq(x, y) => self.skip_if_eq(self.registers[x], self.registers[y]),
+            Instruction::Mov_const(x, nn) => self.registers[x] = nn,
+            Instruction::Add_const(x, nn) => self.registers[x] += nn,
+            Instruction::Mov(x, y) => self.registers[x] = self.registers[y],
+            Instruction::Or(x, y) => self.registers[x] |= self.registers[y],
+            Instruction::And(x, y) => self.registers[x] &= self.registers[y],
+            Instruction::Xor(x, y) => self.registers[x] ^= self.registers[y],
+            Instruction::Add(x, y) => self.registers[x] += self.registers[y],
+            Instruction::Sub(x, y) => self.registers[x] -= self.registers[y],
+            Instruction::Shr(x) => self.shift_right(x),
+            Instruction::Rsb(x, y) => self.registers[x] = (self.registers[y] - self.registers[x]),
+            Instruction::Shl(x) => self.shift_left(x),
+            Instruction::Skne(x, y) => self.skip_if_not_eq(self.registers[x], self.registers[y]),
             Instruction::Mvi(nnn) => self.index = nnn,
+            Instruction::Jmi(nnn) => self.pc = (nnn + self.registers[0] as usize),
+            Instruction::Rand(x, nn) => self.rand(x, nn),
             Instruction::Sprite(x, y, n) => self.execute_sprite(x, y, n),
+            Instruction::Skpr(x) => if self.keypad.is_pressed(x) {self.pc += 2},
+            Instruction::Skup(x) => if !self.keypad.is_pressed(x) {self.pc += 2},
+            Instruction::Gdelay(x) => self.registers[x] = self.delay_timer,
+            Instruction::Key(x) => self.execute_key(x),
+            Instruction::Sdelay(x) => self.delay_timer = self.registers[x],
+            Instruction::Ssound(x) => self.sound_timer = self.registers[x],
+            Instruction::Adi(x) => self.index += self.registers[x] as usize,
+
 
             Instruction::None => panic!("Tried to execute Instruction::None!"),
             _ => panic!("Tried to execute unsupported instruction!"),
@@ -82,11 +115,60 @@ impl Chip8 {
     fn set_vf(&mut self, value: u8) {
         self.registers[0xF] = value;
     }
+
+    fn push(&mut self, value: u16) {
+        if self.sp < Self::STACK_SIZE {
+            self.stack[self.sp] = value;
+            self.sp += 1;
+        }
+    }
+
+    fn pop(&mut self) -> u16 {
+        if self.sp > 0 {
+            self.sp -= 1;
+            self.stack[self.sp];
+        }
+        0
+    }
 }
 
 
 // CPU execute helper functions
 impl Chip8 {
+    fn execute_jsr(&mut self, nnn: usize) {
+        self.push(self.pc as u16);
+        self.pc = nnn;
+    }
+
+    fn skip_if_eq(&mut self, x: u8, y: u8) {
+        if x == y {
+            self.pc += 2;
+        }
+    }
+
+    fn skip_if_not_eq(&mut self, x: u8, y: u8) {
+        if x != y {
+            self.pc += 2;
+        }
+    }
+
+    fn shift_right(&mut self, x: usize) {
+        // Store bit 0 in VF
+        self.set_vf((x & 0x01) as u8);
+        self.registers[x] >> 1;
+    }
+
+    fn shift_left(&mut self, x: usize) {
+        // Store bit 7 in VF
+        self.set_vf((x & 0x80) as u8);
+        self.registers[x] << 1;
+    }
+
+    fn rand(&mut self, x: usize, nn: u8) {
+        let rand_num: u8 = rand::random();
+        self.registers[x] = (rand_num & nn);
+    }
+
     fn execute_sprite(&mut self, x: usize, y: usize, n: u8) {
         let x_coord = self.registers[x] as usize;
         let y_coord = self.registers[y] as usize;
@@ -101,4 +183,17 @@ impl Chip8 {
             self.set_vf(1);
         }
     }
+
+    fn execute_key(&mut self, x: usize) {
+        let (key, pressed) = self.keypad.is_pressed_any();
+        if pressed {
+            self.registers[x] = key as u8;
+        } else {
+            self.pc -= 2; // Stay at same instruction
+        }
+    }
+}
+
+struct Stack {
+
 }
--- core/src/opcode.rs
+++ core/src/opcode.rs
@@ -1,11 +1,42 @@
 pub(crate) enum Instruction {
     None,
     Cls,                        //00E0
-    Jmp(u16),                   //1nnn
-    Mov(usize, u8),            //6xnn
-    Add(usize, u8),            //7xnn
-    Mvi(u16),                   //Annn
+    Rts,                        //00EE
+    Low,                        //00FE
+    High,                       //00FF
+    Jmp(usize),                 //1nnn
+    Jsr(usize),                 //2nnn
+    Skeq_const(usize, u8),      //3xnn
+    Skne_const(usize, u8),      //4xnn
+    Skeq(usize, usize),         //5xy0
+    Mov_const(usize, u8),       //6xnn
+    Add_const(usize, u8),       //7xnn
+    Mov(usize, usize),          //8xy0
+    Or(usize, usize),           //8xy1
+    And(usize, usize),          //8xy2
+    Xor(usize, usize),          //8xy3
+    Add(usize, usize),          //8xy4
+    Sub(usize, usize),          //8xy5
+    Shr(usize),                 //8x06
+    Rsb(usize, usize),          //8xy7
+    Shl(usize),                 //8x0E
+    Skne(usize, usize),         //9xy0
+    Mvi(usize),                 //Annn
+    Jmi(usize),                 //Bnnn
+    Rand(usize, u8),            //Cxnn
     Sprite(usize, usize, u8),   //Dxyn
+    Skpr(usize),                //Ex9E
+    Skup(usize),                //ExA1
+    Gdelay(usize),              //Fx07
+    Key(usize),                 //Fx0A
+    Sdelay(usize),              //Fx15
+    Ssound(usize),              //Fx18
+    Adi(usize),                 //Fx1E
+    Font(usize),                //Fx29
+    Xfont(usize),               //Fx30
+    Bcd(usize),                 //Fx33
+    Str(usize),                 //Fx55
+    Ldr(usize),                 //Fx65
 }
 
 fn extract_nibbles(opcode: u16, position: u8, length: u8) -> u16 {
@@ -18,18 +49,63 @@ pub(crate) fn decode (opcode: u16) -> Instruction {
     let y       = extract_nibbles(opcode, 1, 1) as usize;
     let n       = extract_nibbles(opcode, 0, 1) as u8;
     let nn      = extract_nibbles(opcode, 0, 2) as u8;
-    let nnn     = extract_nibbles(opcode, 0, 3);
+    let nnn     = extract_nibbles(opcode, 0, 3) as usize;
 
     match nibble {
-        0x0 => match y {
-            0xE => Instruction::Cls,
+        0x0 => match nn {
+            0xE0 => Instruction::Cls,
+            0xEE => Instruction::Rts,
+            0xFE => Instruction::Low,
+            0xFF => Instruction::High,
+
             _ => Instruction::None
         }
         0x1 => Instruction::Jmp(nnn),
-        0x6 => Instruction::Mov(x, nn),
-        0x7 => Instruction::Add(x, nn),
+        0x2 => Instruction::Jsr(nnn),
+        0x3 => Instruction::Skeq_const(x, nn),
+        0x4 => Instruction::Skne_const(x, nn),
+        0x5 => Instruction::Skeq(x, y),  
+        0x6 => Instruction::Mov_const(x, nn),
+        0x7 => Instruction::Add_const(x, nn),
+        0x8 => match n {
+            0x0 => Instruction::Mov(x, y),
+            0x1 => Instruction::Or(x, y),
+            0x2 => Instruction::And(x, y),
+            0x3 => Instruction::Xor(x, y),
+            0x4 => Instruction::Add(x, y),
+            0x5 => Instruction::Sub(x, y),
+            0x6 => Instruction::Shr(x),
+            0x7 => Instruction::Rsb(x, y),
+            0xE => Instruction::Shl(x),
+
+            _ => Instruction::None
+        }
+        0x9 => Instruction::Skne(x, y),
         0xA => Instruction::Mvi(nnn),
+        0xB => Instruction::Jmi(nnn),
+        0xC => Instruction::Rand(x, nn),
         0xD => Instruction::Sprite(x, y, n),
+        0xE => match n {
+            0x1 => Instruction::Skup(x),
+            0xE => Instruction::Skpr(x),
+
+            _ => Instruction::None
+        }
+        0xF => match nn {
+            0x07 => Instruction::Gdelay(x),
+            0x0A => Instruction::Key(x),
+            0x15 => Instruction::Sdelay(x),
+            0x18 => Instruction::Ssound(x),
+            0x1E => Instruction::Adi(x),
+            0x29 => Instruction::Font(x),
+            0x30 => Instruction::Xfont(x),
+            0x33 => Instruction::Bcd(x),
+            0x55 => Instruction::Str(x),
+            0x65 => Instruction::Ldr(x),
+
+            _ => Instruction::None
+        }
+
         _ => Instruction::None
     }
 }
