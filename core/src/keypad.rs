0edbb8146a5e21a14bfe88b446a02f8695758b67
magnch <magnus.11387@gmail.com>
2026-08-15 12:27:25 +0200

    Add methods to Keypad struct

--- core/src/keypad.rs
+++ core/src/keypad.rs
@@ -5,8 +5,21 @@ pub struct Keypad {
 impl Keypad {
     const NUM_KEYS: usize = 16;
 
+    pub(crate) fn new() -> Self {
+        Keypad { keys: [false; Self::NUM_KEYS] }
+    }
+
     pub(crate) fn is_pressed(&self, key: usize) -> bool {
         self.keys[key]
+    }
+
+    pub(crate) fn is_pressed_any(&self) -> (usize, bool) {
+        for (key, pressed) in self.keys.iter().enumerate() {
+            if *pressed {
+               return (key, true);
+            }
+        }
+        (0xFF, false)
     }
 
     pub(crate) fn press_key(&mut self, key: usize) {
